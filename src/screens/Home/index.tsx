9cb843d2d297e5c01afe9610e75b9a95f10a9722
Uğur Mamak <ugur.mamak@eteration.com>
2023-09-10 18:41:48 +0300

    react-native-gesture-handler added

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {
+  Text,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+function Home() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Home</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default Home;
---
+++ src/screens/List/index.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {
+  Text,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+function List(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>List</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default List;
